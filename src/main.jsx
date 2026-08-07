d25ceb1159feb8c63e59100c9fd80069f934642b
GYASH28 <yash.k.ganesh@gmail.com>
2026-08-07 11:12:58 +0530

    style: load site-wide experience polish layer

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ import "./production-a11y-fixes.css";
 import "./motion-performance-v5.css";
 import "./hero-v6.css";
 import "./hero-cinematic-v7.css";
+import "./site-experience-v8.css";
 
 applyMotionProfile();
 
