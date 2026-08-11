6b0ed60b878d53e2a36ccb1498a39140315d00b0
GYASH28 <yash.k.ganesh@gmail.com>
2026-08-11 19:45:30 +0530

    data: add verified Fakhri Mart identity and contacts

---
+++ src/data/verifiedBusiness.js
@@ -0,0 +1,11 @@
+export const verifiedBusiness = {
+  tagline: "Colorful Threads, Endless Creation",
+  email: "fakhrimart2025@gmail.com",
+  emailHref: "mailto:fakhrimart2025@gmail.com",
+  phoneDisplay: "+91 88307 37551",
+  phoneHref: "tel:+918830737551",
+  secondaryPhoneDisplay: "+91 96735 21786",
+  secondaryPhoneHref: "tel:+919673521786",
+  whatsappDisplay: "+91 88307 37551",
+  whatsappNumber: "918830737551",
+};
