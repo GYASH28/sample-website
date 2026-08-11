c2beb8c3b1c930c3cfb96b08693c1adddd5faf1f
GYASH28 <yash.k.ganesh@gmail.com>
2026-08-11 21:27:09 +0530

    seo: index project and material collection discovery routes

--- src/scripts/generate-sitemap.js
+++ src/scripts/generate-sitemap.js
@@ -5,11 +5,13 @@ import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const { businessInfo, featuredProducts, blogPosts } = await import("../data/siteData.js");
 const { verifiedBusiness } = await import("../data/verifiedBusiness.js");
+const { SEO_COLLECTIONS } = await import("../data/discoveryData.js");
 Object.assign(businessInfo, verifiedBusiness);
 
 const staticRoutes = [
   "/",
   "/products",
+  "/projects",
   "/yarn-guide",
   "/about",
   "/contact",
@@ -18,14 +20,12 @@ const staticRoutes = [
   "/terms",
   "/delivery-enquiries",
 ];
+const collectionRoutes = SEO_COLLECTIONS.map((collection) => `/collections/${collection.slug}`);
 const productRoutes = featuredProducts.map((product) => `/products/${product.slug}`);
 const blogRoutes = blogPosts.map((post) => `/blog/${post.slug}`);
-const allRoutes = [...staticRoutes, ...productRoutes, ...blogRoutes];
+const allRoutes = [...staticRoutes, ...collectionRoutes, ...productRoutes, ...blogRoutes];
 
-const urls = allRoutes
-  .map((route) => `  <url>\n    <loc>${businessInfo.url}${route}</loc>\n  </url>`)
-  .join("\n");
-
+const urls = allRoutes.map((route) => `  <url>\n    <loc>${businessInfo.url}${route}</loc>\n  </url>`).join("\n");
 const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`;
 
 const outPath = resolve(__dirname, "../../public/sitemap.xml");
