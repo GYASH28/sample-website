f1fd5c74a940aebc7f85737b8cbb138442acf5de
GYASH28 <yash.k.ganesh@gmail.com>
2026-09-15 13:40:52 +0530

    Load responsive polish layer

--- src/main.jsx
+++ src/main.jsx
@@ -45,6 +45,7 @@ import "./guide-about-v22.css";
 import "./liquid-navigation-v23.css";
 import "./interaction-stability-v24.css";
 import "./theme-readability-v25.css";
+import "./mobile-polish-v26.css";
 
 applyCatalogueIntegrity();
 applyMotionProfile();
